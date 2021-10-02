355d970f7ef36473abe6d7a2daed1b4a52c893b7
Scott Zhao <zhaomh1998@outlook.com>
2021-10-02 00:57:32 -0400

    added shebang; parser finished

--- src/seh.py
+++ src/seh.py
@@ -1,33 +1,40 @@
+#!/usr/bin/env python3
 import argparse
 
 if __name__ == '__main__':
     # Parse Arguments
+    available_slots = list(range(1, 5))
+    slots_description = [f'slot{x}' for x in available_slots]
 
     parser = argparse.ArgumentParser(description='SensorHub Controller - Modify and Control SensorHub')
     subparsers = parser.add_subparsers(dest='command', help='Run seh {command} -h for additional help', required=True)
 
-    # seh install <name> slotX
+    # > seh install <name> slotX
     parser_install = subparsers.add_parser(
         'install', help='Install a new SensorHub module into a specific slot')
 
     parser_install.add_argument('ModuleName', help='ModuleName')
-    parser_install.add_argument('InstallTo',
-                                help='slot1/slot2/slot3/slot4', choices=['slot1', 'slot2', 'slot3', 'slot4'])
+    parser_install.add_argument('InstallTo', help='/'.join(slots_description), choices=slots_description)
 
-    # seh uninstall <name> slotX
+    # > seh uninstall <name> slotX
     parser_uninstall = subparsers.add_parser(
         'uninstall', help='Uninstall a new SensorHub module from a specific slot')
-    parser_uninstall.add_argument('UninstallFrom',
-                                  help='slot1/slot2/slot3/slot4', choices=['slot1', 'slot2', 'slot3', 'slot4'])
+    parser_uninstall.add_argument('UninstallFrom', help='/'.join(slots_description), choices=slots_description)
 
-    # seh slotX start/stop/restart
-    parser_slot1_ctl = subparsers.add_parser('slot1', help='start/stop/restart module on slot1')
-    parser_slot2_ctl = subparsers.add_parser('slot2', help='start/stop/restart module on slot2')
-    parser_slot3_ctl = subparsers.add_parser('slot3', help='start/stop/restart module on slot3')
-    parser_slot4_ctl = subparsers.add_parser('slot4', help='start/stop/restart module on slot4')
-    for p in [parser_slot1_ctl, parser_slot2_ctl, parser_slot3_ctl, parser_slot4_ctl]:
-        p.add_argument('Operation', help='start/stop/restart',
-                       choices=['start', 'stop', 'restart'])
+    # > seh slotX start/stop/restart
+    for slot_x in available_slots:
+        parser_slot = subparsers.add_parser(f'slot{slot_x}', help=f'start/stop/restart module on slot{slot_x}')
+        parser_slot.add_argument('Operation', help='start/stop/restart',
+                                 choices=['start', 'stop', 'restart'])
 
     args = parser.parse_args()
-    print(args)
+
+    if args.command == 'install':
+        raise NotImplementedError(f'Install {args.ModuleName} to {args.InstallTo}')
+    elif args.command == 'uninstall':
+        raise NotImplementedError(f'Uninstall {args.ModuleName} from {args.InstallTo}')
+    elif args.command.startswith('slot'):
+        slot = args.command.lstrip('slot')
+        raise NotImplementedError(f'{args.Operation} {slot}')
+    else:
+        raise NotImplementedError(args)
