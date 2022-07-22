9934d50b4bdec40349f84a26ae5c65d98ce1247b
Scott Zhao <zhaomh1998@outlook.com>
2022-07-22 18:48:21 -0400

    add startup script

---
+++ src/startup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+INTERNET_OK=true
+printf "%s" "Waiting for Internet ..."
+while ! ping -c 1 -n -w 1 example.com &> /dev/null
+do
+    sleep 1
+    printf "\nWaiting ... %ds" "$SECONDS"
+    if [[ $SECONDS -gt 30 ]]; then
+        printf "\n%s\n" "Timed out waiting for Internet"
+        INTERNET_OK=false
+        break
+    fi
+done
+if [ "$INTERNET_OK" = true ] ; then
+    printf "\n%s\n"  "System is online. Waiting for time sync..."
+    # Wait for time sync
+    sleep 10
+    printf "$s\n" "Starting services"
+fi
+
+/usr/bin/screen -dmS sensorhub_controller bash -c '/home/pi/workspace/env/shenv/bin/python /home/pi/workspace/SensorHub/src/controller/controller.py; exec bash'
+/usr/bin/screen -dmS sensorhub_service bash -c '/home/pi/workspace/env/shenv/bin/python /home/pi/workspace/SensorHub/src/sehd.py /home/pi/workspace/SensorHub/src/fuse; exec bash'
+/usr/bin/screen -dmS usb_task bash -c '/home/pi/workspace/env/shenv/bin/python /home/pi/workspace/SensorHub/src/usb_task.py; exec bash'
+/usr/bin/screen -dmS frp bash -c '/home/pi/workspace/frp/frpc -c /home/pi/workspace/frp/frpc.ini; exec bash'
